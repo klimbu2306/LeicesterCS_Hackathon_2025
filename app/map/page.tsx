6b3c0bf0ede476cb1bf8b155e193466b19b57874
James Schaffer <james.t.schaffer@gmail.com>
2025-11-22 19:24:45 +0000

    -fixed undefined window issue

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,12 +1,10 @@
-"use client"
-import Map from '../components/Map'
+"use client";
+import dynamic from "next/dynamic";
 
-export default function About() {
-  return (
-    
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <Map />  {/* Render the map component here */}
-    </div>
-  );
+const Map = dynamic(() => import('../components/MapClient'), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <Map />;
 }
